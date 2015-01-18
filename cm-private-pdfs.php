84860dbb42864391e83d79d51b3f187477be7b33
keksters <web2@creativem.co.nz>
2015-01-19 11:35:36 +1300

    Index + gitignore

---
+++ cm-private-pdfs.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Creative Marketing Private PDFs
+ * Plugin URI: http://creativem.co.nz/
+ * Description: Allows for PDF statements to be issued privately to users on website.
+ * Version: 1.0.0
+ * Author: Jessica Christini, Kevin Phillips
+ * Author URI: http://creativem.co.nz/
+ * License: GPL2
+ */
